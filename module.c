b0c04da45dc4226e6d27a74e9556975e327956e1
Krzysztof Foltman <wdev@foltman.com>
2011-01-20 22:22:54 +0000

    Initial implementation of scanner vibrato.

--- module.c
+++ module.c
@@ -9,8 +9,20 @@
 
 // a0 a1 a2 b1 b2 for scanner vibrato filter @4kHz with sr=44.1:    0.057198 0.114396 0.057198 -1.218829 0.447620
 
+static int64_t scanner_a0 = (int64_t)(0.057198 * 1048576);
+static int64_t scanner_b1 = (int64_t)(-1.218829 * 1048576);
+static int64_t scanner_b2 = (int64_t)(0.447620 * 1048576);
+
 static int sine_table[2048];
 static int complex_table[2048];
+
+struct biquad
+{
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+};
 
 struct mono_sine_module
 {
@@ -20,8 +32,12 @@ struct mono_sine_module
     uint32_t phase[91];
     uint32_t keymasks[4];
     int amp_scaling[91];
+    struct biquad scanner_delay[18];
     int lowpass_x1, lowpass_y1;
     float percussion;
+    int do_filter;
+    int cc91;
+    uint32_t vibrato_phase;
 };
 
 static const int drawbars[9] = {0, 19, 12, 24, 24 + 7, 36, 36 + 4, 36 + 7, 48};
@@ -54,6 +70,10 @@ void mono_sine_process_event(void *user_data, const uint8_t *data, uint32_t len)
                 manual_drawbar_settings[data[1] - 21] = data[2] * 8 / 127;
             if (data[1] == 82)
                 manual_drawbar_settings[8] = data[2] * 8 / 127;
+            if (data[1] == 64)
+                m->do_filter = data[2] >= 64;
+            if (data[1] == 91)
+                m->cc91 = data[2];
         }
     }
 }
@@ -92,24 +112,11 @@ inline int tonegenidx(int note, int shift)
     return note + shift;
 }
 
-void mono_sine_process_block(void *user_data, cbox_sample_t **inputs, cbox_sample_t **outputs)
+static void set_tonewheels(struct mono_sine_module *m, int tonegens[92])
 {
-    struct mono_sine_module *m = user_data;
     int n, i;
-    //float a01, b1, x1, y1, x, q;
-    int a01, b1, x1, y1;
     
-    static const uint32_t frac_mask = (1 << 21) - 1;
-    
-    int internal_out[CBOX_BLOCK_SIZE];
-    
-    for (i = 0; i < CBOX_BLOCK_SIZE; i++)
-    {
-        internal_out[i] = 0;
-    }
-    // 91 tonewheels + 1 dummy
-    int tonegens[92];
-    memset(tonegens, 0, sizeof(tonegens));
+    memset(tonegens, 0, 92 * sizeof(tonegens[0]));
     // pedalboard
     for (n = 24; n < 24 + 12; n++)
     {
@@ -136,6 +143,26 @@ void mono_sine_process_block(void *user_data, cbox_sample_t **inputs, cbox_sampl
                 tonegens[tonegenidx(n, 24+7)] += m->percussion * 32;
         }
     }
+}
+
+void mono_sine_process_block(void *user_data, cbox_sample_t **inputs, cbox_sample_t **outputs)
+{
+    struct mono_sine_module *m = user_data;
+    int n, i;
+    //float a01, b1, x1, y1, x, q;
+    int a01, b1, x1, y1;
+    
+    static const uint32_t frac_mask = (1 << 21) - 1;
+    
+    int internal_out[CBOX_BLOCK_SIZE];
+    
+    for (i = 0; i < CBOX_BLOCK_SIZE; i++)
+    {
+        internal_out[i] = 0;
+    }
+    // 91 tonewheels + 1 dummy
+    int tonegens[92];
+    set_tonewheels(m, tonegens);
     if (m->percussion > 0)
         m->percussion *= 0.99f;
     for (n = 0; n < 91; n++)
@@ -189,12 +216,66 @@ void mono_sine_process_block(void *user_data, cbox_sample_t **inputs, cbox_sampl
             out = 32767;
         internal_out[i] = out;
     }
+    int32_t vibrato_dphase = (int)(6.6 / 44100 * 65536 * 65536);
+    for (i = 0; i < CBOX_BLOCK_SIZE; i++)
+    {
+        static const int v1[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 8 };
+        static const int v2[] = { 0, 1, 2, 4, 6, 8, 9, 10, 12 };
+        static const int v3[] = { 0, 1, 3, 6, 11, 12, 15, 17, 18, 18, 18 };
+        const int *dmap = v3;
+        int x0 = internal_out[i];
+        int delay[19];
+        int64_t accum;
+        uint32_t vphase = m->vibrato_phase;
+        uint32_t vphint;
+        if (vphase >= 0x80000000)
+            vphase = ~vphase;
+        delay[0] = x0;
+        for (n = 0; n < 18; n++)
+        {
+            struct biquad *bq = &m->scanner_delay[n];
+            accum = 0;
+            accum += (x0 + (bq->x1 << 1) + bq->x2) * scanner_a0;
+            accum -= bq->y1 * scanner_b1;
+            accum -= bq->y2 * scanner_b2;
+            accum = accum >> 20;
+            bq->x2 = bq->x1;
+            bq->x1 = x0;
+            bq->y2 = bq->y1;
+            bq->y1 = accum;
+            
+            delay[1 + n] = x0 = accum;
+        }
+        m->vibrato_phase += vibrato_dphase;
+        
+        vphint = vphase >> 28;
+        accum = 0;
+        accum += ((int64_t)delay[0]) << 28;
+        accum += delay[dmap[vphint]] * ((1ULL << 28) - (vphase & ~0xF0000000));
+        accum += delay[dmap[vphint + 1]] * (vphase & ~0xF0000000ULL);
+        
+        vphase = m->vibrato_phase + (1 << 27);
+        if (vphase >= 0x80000000)
+            vphase = ~vphase;        
+        vphint = vphase >> 28;
+        accum = 0;
+        accum += ((int64_t)delay[0]) << 28;
+        accum += delay[dmap[vphint]] * ((1ULL << 28) - (vphase & ~0xF0000000));
+        accum += delay[dmap[vphint + 1]] * (vphase & ~0xF0000000ULL);
+        
+        internal_out[i] = accum >> 29;
+    }
     m->lowpass_x1 = x1;
     m->lowpass_y1 = y1;
     for (i = 0; i < CBOX_BLOCK_SIZE; i++)
     {
         outputs[1][i] = outputs[0][i] = internal_out[i] * (1.0 / 32768.0);
     }
+}
+
+static void biquad_init(struct biquad *bq)
+{
+    bq->x1 = bq->y1 = bq->x2 = bq->y2 = 0;
 }
 
 struct cbox_module *mono_sine_create(void *user_data)
@@ -219,6 +300,13 @@ struct cbox_module *mono_sine_create(void *user_data)
     m->lowpass_x1 = 0;
     m->lowpass_y1 = 0;
     m->percussion = -1;
+    m->do_filter = 0;
+    m->cc91 = 0;
+    m->vibrato_phase = 0;
+    for (i = 0; i < 18; i++)
+    {
+        biquad_init(&m->scanner_delay[i]);
+    }
     for (i = 0; i < 91; i++)
     {
         float freq_hz = 440 * pow(2.0, (i - 45) / 12.0);
