5ff4cc8ba380e7b2f835da0c643cb437e96bb60c
Krzysztof Foltman <wdev@foltman.com>
2011-02-19 21:08:52 +0000

    Oops, missing file.

---
+++ module.c
@@ -0,0 +1,27 @@
+/*
+Calf Box, an open source musical instrument.
+Copyright (C) 2010 Krzysztof Foltman
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "module.h"
+
+#include <stdlib.h>
+
+struct cbox_module_manifest *cbox_module_list[] = {
+    &tonewheel_organ_module,
+    &fluidsynth_module,
+    NULL
+};
