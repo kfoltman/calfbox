86b0fb8eb98c0a5db99fd46d07433fe9567bf8a2
Krzysztof Foltman <wdev@foltman.com>
2011-02-26 10:48:49 +0000

    Add missing includes.

--- io.c
+++ io.c
@@ -17,6 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "config.h"
+#include "config-api.h"
 #include "io.h"
 
 #include <stdlib.h>
--- tonewheel.c
+++ tonewheel.c
@@ -16,6 +16,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "config.h"
+#include "config-api.h"
 #include "dspmath.h"
 #include "module.h"
 #include <glib.h>
