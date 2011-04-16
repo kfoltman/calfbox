ff5c5a40209b639eaadc12c421d2842293c59337
Krzysztof Foltman <wdev@foltman.com>
2011-04-16 21:08:20 +0100

    Update dates in some copyright notices.

--- master.h
+++ master.h
@@ -1,6 +1,6 @@
 /*
 Calf Box, an open source musical instrument.
-Copyright (C) 2010 Krzysztof Foltman
+Copyright (C) 2010-2011 Krzysztof Foltman
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
