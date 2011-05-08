dfaafda9be827b1c4454b88b82cf9bbce1ea1892
Krzysztof Foltman <wdev@foltman.com>
2011-05-08 10:03:47 +0100

    Move stream state into a separate structure.
    This is to make it easier to implement stream switching in future.

--- streamplay.c
+++ streamplay.c
@@ -47,10 +47,8 @@ struct stream_player_cue_point
     int queued;
 };
 
-struct stream_player_module
+struct stream_state
 {
-    struct cbox_module module;
-
     SNDFILE *sndfile;
     SF_INFO info;
     uint64_t readptr;
@@ -63,27 +61,33 @@ struct stream_player_module
     struct stream_player_cue_point *pcp_current, *pcp_next;
     
     jack_ringbuffer_t *rb_for_reading, *rb_just_read;
-    
-    pthread_t thr_preload;
-    
     float gain;
 };
 
-static void init_cue(struct stream_player_module *m, struct stream_player_cue_point *pt, uint32_t size, uint64_t pos)
+struct stream_player_module
 {
-    pt->data = malloc(size * sizeof(float) * m->info.channels);
+    struct cbox_module module;
+    
+    pthread_t thr_preload;
+    
+    struct stream_state *stream;
+};
+
+static void init_cue(struct stream_state *ss, struct stream_player_cue_point *pt, uint32_t size, uint64_t pos)
+{
+    pt->data = malloc(size * sizeof(float) * ss->info.channels);
     pt->size = size;
     pt->length = 0;
     pt->queued = 0;
     pt->position = pos;
 }
 
-static void load_at_cue(struct stream_player_module *m, struct stream_player_cue_point *pt)
+static void load_at_cue(struct stream_state *ss, struct stream_player_cue_point *pt)
 {
     if (pt->position != NO_SAMPLE_LOOP)
     {
-        sf_seek(m->sndfile, pt->position, 0);
-        pt->length = sf_readf_float(m->sndfile, pt->data, pt->size);
+        sf_seek(ss->sndfile, pt->position, 0);
+        pt->length = sf_readf_float(ss->sndfile, pt->data, pt->size);
     }
     pt->queued = 0;
 }
@@ -98,51 +102,51 @@ static int is_queued(struct stream_player_cue_point *pt, uint64_t ofs)
     return pt->queued && pt->position != NO_SAMPLE_LOOP && ofs >= pt->position && ofs < pt->position + pt->size;
 }
 
-struct stream_player_cue_point *get_cue(struct stream_player_module *m, uint64_t pos)
+struct stream_player_cue_point *get_cue(struct stream_state *ss, uint64_t pos)
 {
     int i;
     
-    if (is_contained(&m->cp_loop, pos))
-        return &m->cp_loop;
-    if (is_contained(&m->cp_start, pos))
-        return &m->cp_start;
+    if (is_contained(&ss->cp_loop, pos))
+        return &ss->cp_loop;
+    if (is_contained(&ss->cp_start, pos))
+        return &ss->cp_start;
     
     for (i = 0; i < MAX_READAHEAD_BUFFERS; i++)
     {
-        if (m->cp_readahead_ready[i] && is_contained(&m->cp_readahead[i], pos))
-            return &m->cp_readahead[i];
+        if (ss->cp_readahead_ready[i] && is_contained(&ss->cp_readahead[i], pos))
+            return &ss->cp_readahead[i];
     }
     return NULL;
 }
 
-struct stream_player_cue_point *get_queued_buffer(struct stream_player_module *m, uint64_t pos)
+struct stream_player_cue_point *get_queued_buffer(struct stream_state *ss, uint64_t pos)
 {
     int i;
     
     for (i = 0; i < MAX_READAHEAD_BUFFERS; i++)
     {
-        if (!m->cp_readahead_ready[i] && is_queued(&m->cp_readahead[i], pos))
-            return &m->cp_readahead[i];
+        if (!ss->cp_readahead_ready[i] && is_queued(&ss->cp_readahead[i], pos))
+            return &ss->cp_readahead[i];
     }
     return NULL;
 }
 
-void request_load(struct stream_player_module *m, int buf_idx, uint64_t pos)
+void request_load(struct stream_state *ss, int buf_idx, uint64_t pos)
 {
     unsigned char cidx = (unsigned char)buf_idx;
-    struct stream_player_cue_point *pt = &m->cp_readahead[buf_idx];
+    struct stream_player_cue_point *pt = &ss->cp_readahead[buf_idx];
     int wlen = 0;
     
-    m->cp_readahead_ready[buf_idx] = 0;    
+    ss->cp_readahead_ready[buf_idx] = 0;    
     pt->position = pos;
     pt->length = 0;
     pt->queued = 1;
 
-    wlen = jack_ringbuffer_write(m->rb_for_reading, &cidx, 1);
+    wlen = jack_ringbuffer_write(ss->rb_for_reading, &cidx, 1);
     assert(wlen);
 }
 
-int get_unused_buffer(struct stream_player_module *m)
+int get_unused_buffer(struct stream_state *ss)
 {
     int i = 0;
     int notbad = -1;
@@ -151,16 +155,16 @@ int get_unused_buffer(struct stream_player_module *m)
     for (i = 0; i < MAX_READAHEAD_BUFFERS; i++)
     {
         int64_t rel;
-        if (&m->cp_readahead[i] == m->pcp_current)
+        if (&ss->cp_readahead[i] == ss->pcp_current)
             continue;
-        if (m->cp_readahead[i].queued)
+        if (ss->cp_readahead[i].queued)
             continue;
         // If there's any unused buffer, return it
-        if (m->cp_readahead[i].position == NO_SAMPLE_LOOP)
+        if (ss->cp_readahead[i].position == NO_SAMPLE_LOOP)
             return i;
         // If this has already been played, return it
-        rel = m->readptr - m->cp_readahead[i].position;
-        if (rel >= m->cp_readahead[i].length)
+        rel = ss->readptr - ss->cp_readahead[i].position;
+        if (rel >= ss->cp_readahead[i].length)
             return i;
         // Use as second chance
         notbad = i;
@@ -171,10 +175,11 @@ int get_unused_buffer(struct stream_player_module *m)
 void *sample_preload_thread(void *user_data)
 {
     struct stream_player_module *m = user_data;
+    struct stream_state *ss = m->stream;
     
     do {
         unsigned char buf_idx;
-        if (!jack_ringbuffer_read(m->rb_for_reading, &buf_idx, 1))
+        if (!jack_ringbuffer_read(ss->rb_for_reading, &buf_idx, 1))
         {
             usleep(5000);
             continue;
@@ -182,9 +187,9 @@ void *sample_preload_thread(void *user_data)
         if (buf_idx == 255)
             break;
         // fprintf(stderr, "Preload: %d, %lld\n", (int)buf_idx, (long long)m->cp_readahead[buf_idx].position);
-        load_at_cue(m, &m->cp_readahead[buf_idx]);
+        load_at_cue(ss, &ss->cp_readahead[buf_idx]);
         // fprintf(stderr, "Preloaded\n", (int)buf_idx, (long long)m->cp_readahead[buf_idx].position);
-        jack_ringbuffer_write(m->rb_just_read, &buf_idx, 1);
+        jack_ringbuffer_write(ss->rb_just_read, &buf_idx, 1);
     } while(1);
         
 }
@@ -194,7 +199,7 @@ void stream_player_process_event(struct cbox_module *module, const uint8_t *data
     struct stream_player_module *m = (struct stream_player_module *)module;
 }
 
-static void request_next(struct stream_player_module *m, uint64_t pos)
+static void request_next(struct stream_state *ss, uint64_t pos)
 {
     // Check if we've requested a next buffer, if not, request it
     
@@ -202,7 +207,7 @@ static void request_next(struct stream_player_module *m, uint64_t pos)
     // XXXKF This is technically incorrect, it won't tell whether the next "block" that's there
     // isn't actually a single sample. I worked it around by ensuring end of blocks are always
     // at CUE_BUFFER_SIZE boundary, and this works well, but causes buffers to be of uneven size.
-    if (m->pcp_next && (is_contained(m->pcp_next, pos) || is_queued(m->pcp_next, pos)))
+    if (ss->pcp_next && (is_contained(ss->pcp_next, pos) || is_queued(ss->pcp_next, pos)))
     {
         // We're still waiting for the requested buffer, but that's OK
         return;
@@ -210,29 +215,29 @@ static void request_next(struct stream_player_module *m, uint64_t pos)
     
     // We don't know the next buffer, or the next buffer doesn't contain
     // the sample we're looking for.
-    m->pcp_next = get_queued_buffer(m, pos);
-    if (!m->pcp_next)
+    ss->pcp_next = get_queued_buffer(ss, pos);
+    if (!ss->pcp_next)
     {
         // It hasn't even been requested - request it
-        int buf_idx = get_unused_buffer(m);
+        int buf_idx = get_unused_buffer(ss);
         if(buf_idx == -1)
         {
             printf("Ran out of buffers\n");
             return;
         }
-        request_load(m, buf_idx, pos);
-        m->pcp_next = &m->cp_readahead[buf_idx];
+        request_load(ss, buf_idx, pos);
+        ss->pcp_next = &ss->cp_readahead[buf_idx];
         
         // printf("@%lld: Requested load into buffer %d at %lld\n", (long long)m->readptr, buf_idx, (long long) pos);
     }
 }
 
-static void copy_samples(struct stream_player_module *m, cbox_sample_t **outputs, float *data, int count, int ofs, int pos)
+static void copy_samples(struct stream_state *ss, cbox_sample_t **outputs, float *data, int count, int ofs, int pos)
 {
     int i;
-    float gain = m->gain;
+    float gain = ss->gain;
     
-    if (m->info.channels == 1)
+    if (ss->info.channels == 1)
     {
         for (i = 0; i < count; i++)
         {
@@ -240,7 +245,7 @@ static void copy_samples(struct stream_player_module *m, cbox_sample_t **outputs
         }
     }
     else
-    if (m->info.channels == 2)
+    if (ss->info.channels == 2)
     {
         for (i = 0; i < count; i++)
         {
@@ -251,7 +256,7 @@ static void copy_samples(struct stream_player_module *m, cbox_sample_t **outputs
     }
     else
     {
-        uint32_t ch = m->info.channels;
+        uint32_t ch = ss->info.channels;
         for (i = 0; i < count; i++)
         {
             outputs[0][ofs + i] = gain * data[pos * ch];
@@ -259,20 +264,21 @@ static void copy_samples(struct stream_player_module *m, cbox_sample_t **outputs
             pos++;
         }
     }
-    m->readptr += count;
-    if (m->readptr >= (uint32_t)m->info.frames)
+    ss->readptr += count;
+    if (ss->readptr >= (uint32_t)ss->info.frames)
     {
-        m->readptr = m->restart;
+        ss->readptr = ss->restart;
     }
 }
 
 void stream_player_process_block(struct cbox_module *module, cbox_sample_t **inputs, cbox_sample_t **outputs)
 {
     struct stream_player_module *m = (struct stream_player_module *)module;
+    struct stream_state *ss = m->stream;
     int i, optr;
     unsigned char buf_idx;
     
-    if (m->readptr == NO_SAMPLE_LOOP)
+    if (ss->readptr == NO_SAMPLE_LOOP)
     {
         for (int i = 0; i < CBOX_BLOCK_SIZE; i++)
         {
@@ -282,54 +288,54 @@ void stream_player_process_block(struct cbox_module *module, cbox_sample_t **inp
     }
 
     // receive buffer completion messages from the queue
-    while(jack_ringbuffer_read(m->rb_just_read, &buf_idx, 1))
+    while(jack_ringbuffer_read(ss->rb_just_read, &buf_idx, 1))
     {
-        m->cp_readahead_ready[buf_idx] = 1;
+        ss->cp_readahead_ready[buf_idx] = 1;
     }
     
     optr = 0;
     do {
-        if (m->readptr == NO_SAMPLE_LOOP)
+        if (ss->readptr == NO_SAMPLE_LOOP)
             break;
 
-        if (m->pcp_current && !is_contained(m->pcp_current, m->readptr))
-            m->pcp_current = NULL;
+        if (ss->pcp_current && !is_contained(ss->pcp_current, ss->readptr))
+            ss->pcp_current = NULL;
         
-        if (!m->pcp_current)
+        if (!ss->pcp_current)
         {
-            if (m->pcp_next && is_contained(m->pcp_next, m->readptr))
+            if (ss->pcp_next && is_contained(ss->pcp_next, ss->readptr))
             {
-                m->pcp_current = m->pcp_next;
-                m->pcp_next = NULL;
+                ss->pcp_current = ss->pcp_next;
+                ss->pcp_next = NULL;
             }
             else
-                m->pcp_current = get_cue(m, m->readptr);
+                ss->pcp_current = get_cue(ss, ss->readptr);
         }
         
-        if (!m->pcp_current)
+        if (!ss->pcp_current)
         {
-            printf("Underrun at %d\n", (int)m->readptr);
+            printf("Underrun at %d\n", (int)ss->readptr);
             // Underrun; request/wait for next block and output zeros
-            request_next(m, m->readptr);
+            request_next(ss, ss->readptr);
             break;
         }
-        assert(!m->pcp_current->queued);
+        assert(!ss->pcp_current->queued);
         
-        uint64_t data_end = m->pcp_current->position + m->pcp_current->length;
-        uint32_t data_left = data_end - m->readptr;
+        uint64_t data_end = ss->pcp_current->position + ss->pcp_current->length;
+        uint32_t data_left = data_end - ss->readptr;
         
         // If we're close to running out of space, prefetch the next bit
-        if (data_left < PREFETCH_THRESHOLD && data_end < m->info.frames)
-            request_next(m, data_end);
+        if (data_left < PREFETCH_THRESHOLD && data_end < ss->info.frames)
+            request_next(ss, data_end);
         
-        float *data = m->pcp_current->data;
-        uint32_t pos = m->readptr - m->pcp_current->position;
-        uint32_t count = data_end - m->readptr;
+        float *data = ss->pcp_current->data;
+        uint32_t pos = ss->readptr - ss->pcp_current->position;
+        uint32_t count = data_end - ss->readptr;
         if (count > CBOX_BLOCK_SIZE - optr)
             count = CBOX_BLOCK_SIZE - optr;
         
         // printf("Copy samples: copying %d, optr %d, %lld = %d @ [%lld - %lld], left %d\n", count, optr, (long long)m->readptr, pos, (long long)m->pcp_current->position, (long long)data_end, (int)data_left);
-        copy_samples(m, outputs, data, count, optr, pos);
+        copy_samples(ss, outputs, data, count, optr, pos);
         optr += count;
     } while(optr < CBOX_BLOCK_SIZE);
     
@@ -344,12 +350,12 @@ void stream_player_destroy(struct cbox_module *module)
     struct stream_player_module *m = (struct stream_player_module *)module;
     unsigned char cmd = 255;
     
-    jack_ringbuffer_write(m->rb_for_reading, &cmd, 1);
+    jack_ringbuffer_write(m->stream->rb_for_reading, &cmd, 1);
     pthread_join(m->thr_preload, NULL);
     
-    jack_ringbuffer_free(m->rb_for_reading);
-    jack_ringbuffer_free(m->rb_just_read);
-    sf_close(m->sndfile);
+    jack_ringbuffer_free(m->stream->rb_for_reading);
+    jack_ringbuffer_free(m->stream->rb_just_read);
+    sf_close(m->stream->sndfile);
 }
 
 struct cbox_module *stream_player_create(void *user_data, const char *cfg_section, int srate)
@@ -375,37 +381,37 @@ struct cbox_module *stream_player_create(void *user_data, const char *cfg_sectio
     m->module.process_block = stream_player_process_block;
     m->module.destroy = stream_player_destroy;
     
-    m->sndfile = sf_open(filename, SFM_READ, &m->info);
+    m->stream = malloc(sizeof(struct stream_state));
+    struct stream_state *stream = m->stream;
+    stream->sndfile = sf_open(filename, SFM_READ, &stream->info);
     
-    if (!m->sndfile)
+    if (!stream->sndfile)
     {
         g_error("%s: cannot open file '%s': %s", cfg_section, filename, sf_strerror(NULL));
         return NULL;
     }
-    g_message("Frames %d channels %d", (int)m->info.frames, (int)m->info.channels);
+    g_message("Frames %d channels %d", (int)stream->info.frames, (int)stream->info.channels);
     
-    m->rb_for_reading = jack_ringbuffer_create(MAX_READAHEAD_BUFFERS + 1);
-    m->rb_just_read = jack_ringbuffer_create(MAX_READAHEAD_BUFFERS + 1);
+    stream->rb_for_reading = jack_ringbuffer_create(MAX_READAHEAD_BUFFERS + 1);
+    stream->rb_just_read = jack_ringbuffer_create(MAX_READAHEAD_BUFFERS + 1);
     
-    m->readptr = 0;
-    m->restart = (uint64_t)(int64_t)cbox_config_get_int(cfg_section, "loop", -1);
-    m->pcp_current = &m->cp_start;
-    // for testing
-    m->pcp_current = NULL;
-    m->pcp_next = NULL;
-    m->gain = pow(2.0, cbox_config_get_float(cfg_section, "gain", 0.f) / 6.0);
+    stream->readptr = 0;
+    stream->restart = (uint64_t)(int64_t)cbox_config_get_int(cfg_section, "loop", -1);
+    stream->pcp_current = &stream->cp_start;
+    stream->pcp_next = NULL;
+    stream->gain = pow(2.0, cbox_config_get_float(cfg_section, "gain", 0.f) / 6.0);
     
-    init_cue(m, &m->cp_start, CUE_BUFFER_SIZE, 0);
-    load_at_cue(m, &m->cp_start);
-    if (m->restart > 0 && (m->restart % CUE_BUFFER_SIZE) > 0)
-        init_cue(m, &m->cp_loop, CUE_BUFFER_SIZE + (CUE_BUFFER_SIZE - (m->restart % CUE_BUFFER_SIZE)), m->restart);
+    init_cue(stream, &stream->cp_start, CUE_BUFFER_SIZE, 0);
+    load_at_cue(stream, &stream->cp_start);
+    if (stream->restart > 0 && (stream->restart % CUE_BUFFER_SIZE) > 0)
+        init_cue(stream, &stream->cp_loop, CUE_BUFFER_SIZE + (CUE_BUFFER_SIZE - (stream->restart % CUE_BUFFER_SIZE)), stream->restart);
     else
-        init_cue(m, &m->cp_loop, CUE_BUFFER_SIZE, m->restart);
-    load_at_cue(m, &m->cp_loop);
+        init_cue(stream, &stream->cp_loop, CUE_BUFFER_SIZE, stream->restart);
+    load_at_cue(stream, &stream->cp_loop);
     for (i = 0; i < MAX_READAHEAD_BUFFERS; i++)
     {
-        init_cue(m, &m->cp_readahead[i], CUE_BUFFER_SIZE, NO_SAMPLE_LOOP);
-        m->cp_readahead_ready[i] = 0;
+        init_cue(stream, &stream->cp_readahead[i], CUE_BUFFER_SIZE, NO_SAMPLE_LOOP);
+        stream->cp_readahead_ready[i] = 0;
     }
     
     if (pthread_create(&m->thr_preload, NULL, sample_preload_thread, m))
