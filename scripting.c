8ad3a80934b99c262b99a124ab303b521907e1a4
Krzysztof Foltman <wdev@foltman.com>
2011-05-29 01:47:03 +0100

    Implement calling of OSC-style commands from Python, no feedback support yet.

--- scripting.c
+++ scripting.c
@@ -19,12 +19,61 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "app.h"
 #include "errors.h"
 #include "scripting.h"
+#include <assert.h>
 #include <glib.h>
 #include <Python.h>
 
 static PyObject *cbox_python_do_cmd(PyObject *self, PyObject *args)
 {
-    return NULL;
+    const char *command = NULL;
+    const char *ret_path = NULL;
+    PyObject *list = NULL;
+    if (!PyArg_ParseTuple(args, "szO!:do_cmd", &command, &ret_path, &PyList_Type, &list))
+        return NULL;
+    
+    int len = PyList_Size(list);
+    void *extra = malloc(len * sizeof(double));
+    struct cbox_osc_command cmd;
+    GError *error = NULL;
+    char *arg_types = malloc(len + 1);
+    void **arg_values = malloc(len * sizeof(void *));
+    cmd.command = command;
+    cmd.arg_types = arg_types;
+    cmd.arg_values = arg_values;
+    double *arg_space = extra;
+    for (int i = 0; i < len; i++)
+    {
+        cmd.arg_values[i] = &arg_space[i];
+        PyObject *value = PyList_GetItem(list, i);
+        
+        if (PyInt_Check(value))
+        {
+            arg_types[i] = 'i';
+            *(int *)arg_values[i] = PyInt_AsLong(value);
+        }
+        else
+        if (PyFloat_Check(value))
+        {
+            arg_types[i] = 'f';
+            *(double *)arg_values[i] = PyFloat_AsDouble(value);
+        }
+        else
+        if (PyString_Check(value))
+        {
+            arg_types[i] = 's';
+            arg_values[i] = PyString_AsString(value);
+        }
+        else
+            assert(0);
+    }
+    arg_types[len] = '\0';
+    // cbox_osc_command_dump(&cmd);
+    app.cmd_target.process_cmd(&app.cmd_target, NULL, &cmd, &error);
+    free(arg_space);
+    free(arg_values);
+    free(arg_types);
+    
+    Py_RETURN_NONE;
 }
 
 static PyMethodDef EmbMethods[] = {
