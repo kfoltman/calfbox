693b07e7c1d883f791ed28beef1662041cceb3a2
Krzysztof Foltman <wdev@foltman.com>
2011-06-03 23:49:29 +0100

    Implement a bit of scripting API in the phaser effect.

--- phaser.c
+++ phaser.c
@@ -16,11 +16,13 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "onepole-float.h"
+#include "app.h"
 #include "config.h"
 #include "config-api.h"
 #include "dspmath.h"
 #include "module.h"
+#include "onepole-float.h"
+#include "procmain.h"
 #include <glib.h>
 #include <malloc.h>
 #include <math.h>
@@ -31,6 +33,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define NO_STAGES 12
 
+struct phaser_params
+{
+    float center;
+    float mdepth;
+    float fb_amt;
+    float lfo_freq;
+    float sphase;
+    int stages;
+};
+
 struct phaser_module
 {
     struct cbox_module module;
@@ -39,14 +51,45 @@ struct phaser_module
     struct cbox_onepolef_coeffs coeffs[2];
     float fb[2];
     float tpdsr;
+    struct phaser_params *params;
     
-    float center;
-    float mdepth;
-    float fb_amt;
-    float lfo_freq;
-    float phase, sphase;
-    int stages;
+    float phase;
 };
+
+static struct phaser_params *copy_params(struct phaser_module *m)
+{
+    struct phaser_params *res = malloc(sizeof(struct phaser_params));
+    memcpy(res, m->params, sizeof(struct phaser_params));
+    return res;
+}
+
+gboolean phaser_process_cmd(struct cbox_command_target *ct, struct cbox_command_target *fb, struct cbox_osc_command *cmd, GError **error)
+{
+    struct phaser_module *m = (struct phaser_module *)ct->user_data;
+    if (!strcmp(cmd->command, "/center_freq") && !strcmp(cmd->arg_types, "f"))
+    {
+        struct phaser_params *pp = copy_params(m);
+        pp->center = *(float *)cmd->arg_values[0];
+        free(cbox_rt_swap_pointers(app.rt, (void **)&m->params, pp));
+    }
+    else if (!strcmp(cmd->command, "/status") && !strcmp(cmd->arg_types, ""))
+    {
+        if (!cbox_check_fb_channel(fb, cmd->command, error))
+            return FALSE;
+        return cbox_execute_on(fb, NULL, "/center_freq", "f", error, m->params->center) &&
+            cbox_execute_on(fb, NULL, "/mod_depth", "f", error, m->params->mdepth) &&
+            cbox_execute_on(fb, NULL, "/fb_amt", "f", error, m->params->fb_amt) &&
+            cbox_execute_on(fb, NULL, "/lfo_freq", "f", error, m->params->lfo_freq) &&
+            cbox_execute_on(fb, NULL, "/stereo_phase", "f", error, m->params->sphase) &&
+            cbox_execute_on(fb, NULL, "/stages", "i", error, m->params->stages);
+    }
+    else
+    {
+        g_set_error(error, CBOX_MODULE_ERROR, CBOX_MODULE_ERROR_FAILED, "Unknown command '%s'", cmd->command);
+        return FALSE;
+    }
+    return TRUE;
+}
 
 void phaser_process_event(struct cbox_module *module, const uint8_t *data, uint32_t len)
 {
@@ -56,16 +99,17 @@ void phaser_process_event(struct cbox_module *module, const uint8_t *data, uint3
 void phaser_process_block(struct cbox_module *module, cbox_sample_t **inputs, cbox_sample_t **outputs)
 {
     struct phaser_module *m = (struct phaser_module *)module;
+    struct phaser_params *p = m->params;
     int s, c, i;
-    int stages = m->stages;
-    float fb_amt = m->fb_amt;
+    int stages = p->stages;
+    float fb_amt = p->fb_amt;
     float phase = 0;
     if (stages < 0 || stages > NO_STAGES)
         stages = 0;
 
-    cbox_onepolef_set_allpass(&m->coeffs[0], m->tpdsr * (m->center + m->mdepth * sin(m->phase)));
-    cbox_onepolef_set_allpass(&m->coeffs[1], m->tpdsr * (m->center + m->mdepth * sin(m->phase + m->sphase)));
-    m->phase += m->lfo_freq * CBOX_BLOCK_SIZE * m->tpdsr;
+    cbox_onepolef_set_allpass(&m->coeffs[0], m->tpdsr * (p->center + p->mdepth * sin(m->phase)));
+    cbox_onepolef_set_allpass(&m->coeffs[1], m->tpdsr * (p->center + p->mdepth * sin(m->phase + p->sphase)));
+    m->phase += p->lfo_freq * CBOX_BLOCK_SIZE * m->tpdsr;
     
     for (c = 0; c < 2; c++)
     {
@@ -95,14 +139,17 @@ struct cbox_module *phaser_create(void *user_data, const char *cfg_section, int 
     cbox_module_init(&m->module, m, 2, 2);
     m->module.process_event = phaser_process_event;
     m->module.process_block = phaser_process_block;
+    m->module.cmd_target.process_cmd = phaser_process_cmd;
     m->tpdsr = 2.0 * M_PI / srate;
     m->phase = 0;
-    m->sphase = cbox_config_get_float(cfg_section, "stereo_phase", 90.f) * 2 * M_PI / 360;
-    m->lfo_freq = cbox_config_get_float(cfg_section, "lfo_freq", 1.f);
-    m->center = cbox_config_get_float(cfg_section, "center_freq", 1500.f);
-    m->mdepth = cbox_config_get_float(cfg_section, "mod_depth", 500.f);
-    m->fb_amt = cbox_config_get_float(cfg_section, "feedback", 0.f);
-    m->stages = cbox_config_get_int(cfg_section, "stages", NO_STAGES);
+    struct phaser_params *p = malloc(sizeof(struct phaser_params));
+    m->params = p;
+    p->sphase = cbox_config_get_float(cfg_section, "stereo_phase", 90.f) * 2 * M_PI / 360;
+    p->lfo_freq = cbox_config_get_float(cfg_section, "lfo_freq", 1.f);
+    p->center = cbox_config_get_float(cfg_section, "center_freq", 1500.f);
+    p->mdepth = cbox_config_get_float(cfg_section, "mod_depth", 500.f);
+    p->fb_amt = cbox_config_get_float(cfg_section, "feedback", 0.f);
+    p->stages = cbox_config_get_int(cfg_section, "stages", NO_STAGES);
     
     for (b = 0; b < NO_STAGES; b++)
         for (c = 0; c < 2; c++)
