e3143efcd401f63fba72b6e4d3752b653169de30
Krzysztof Foltman <wdev@foltman.com>
2011-06-12 13:35:01 +0100

    Implement UI for reverb settings.

--- example.py
+++ example.py
@@ -170,6 +170,18 @@ class ChorusWindow(PluginWindow):
         add_slider_row(t, 4, "Wet/dry", self.path, values, "wet_dry", 0, 1)
         self.add(t)
 
+class ReverbWindow(PluginWindow):
+    def __init__(self, instrument, output, main_window):
+        PluginWindow.__init__(self, instrument, output, "Reverb", main_window)
+        values = GetThings(self.path + "/status", ["decay_time", "dry_amt", "wet_amt", "lowpass", "highpass"], [])
+        t = gtk.Table(2, 5)
+        add_slider_row(t, 0, "Decay time", self.path, values, "decay_time", 500, 5000)
+        add_slider_row(t, 1, "Dry amount", self.path, values, "dry_amt", -100, 12)
+        add_slider_row(t, 2, "Wet amount", self.path, values, "wet_amt", -100, 12)
+        add_slider_row(t, 3, "Lowpass", self.path, values, "lowpass", 300, 20000)
+        add_slider_row(t, 4, "Highpass", self.path, values, "highpass", 30, 2000)
+        self.add(t)
+
 class FBRWindow(PluginWindow):
     def __init__(self, instrument, output, main_window):
         PluginWindow.__init__(self, instrument, output, "Feedback Reducer", main_window)
@@ -204,6 +216,7 @@ class FBRWindow(PluginWindow):
 engine_window_map = {
     'phaser': PhaserWindow,
     'chorus': ChorusWindow,
+    'reverb' : ReverbWindow,
     'feedback_reducer': FBRWindow,
     'stream_player' : StreamWindow,
     'fluidsynth' : FluidsynthWindow
