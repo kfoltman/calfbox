63a6bbc2a9fbc0f2eb0939c49fbf137757907313
Krzysztof Foltman <wdev@foltman.com>
2011-06-12 12:36:22 +0100

    Make the UI more usable on netbook screens. Refactor the signal handlers.

--- example.py
+++ example.py
@@ -14,28 +14,18 @@ def bold_label(text, halign = 1):
     return l
 
 def left_label(text):
-    l = gtk.Label()
-    l.set_markup(text)
+    l = gtk.Label(text)
     l.set_alignment(0, 0.5)
     return l
 
-def effect_value_changed_int(adjustment, path):
-    cbox.do_cmd(path, None, [int(adjustment.get_value())])
+def checkbox_changed_bool(adjustment, path, *items):
+    cbox.do_cmd(path, None, list(items) + [1 if adjustment.get_active() else 0])
 
-def effect_value_changed_float(adjustment, path):
-    cbox.do_cmd(path, None, [float(adjustment.get_value())])
+def adjustment_changed_int(adjustment, path, *items):
+    cbox.do_cmd(path, None, list(items) + [int(adjustment.get_value())])
 
-def effect_value_changed_array_int(value, path, item):
-    cbox.do_cmd(path, None, [int(item), int(value)])
-
-def effect_value_changed_array_float(value, path, item):
-    cbox.do_cmd(path, None, [int(item), float(value)])
-
-def gain_value_changed(adjustment, instr, output_pair):
-    cbox.do_cmd("/instr/%s/set_gain" % instr, None, [output_pair, adjustment.get_value()])
-
-def tempo_value_changed(adjustment):
-    cbox.do_cmd("/master/set_tempo", None, [adjustment.get_value()])
+def adjustment_changed_float(adjustment, path, *items):
+    cbox.do_cmd(path, None, list(items) + [float(adjustment.get_value())])
 
 def output_combo_value_changed(combo, instr, output_pair):
     if combo.get_active() != -1:
@@ -138,19 +128,25 @@ class FluidsynthWindow(gtk.VBox):
         print "combo: %s channel: %s" % (combo.get_active(), channel)
         cbox.do_cmd(self.path + "/set_patch", None, [int(channel), int(self.patches[combo.get_active()][1])])
 
-def add_slider_row(t, row, label, path, values, item, min, max, setter = effect_value_changed_float):
+def add_slider_row(t, row, label, path, values, item, min, max, setter = adjustment_changed_float):
     t.attach(bold_label(label), 0, 1, row, row+1, gtk.SHRINK | gtk.FILL)
     adj = gtk.Adjustment(getattr(values, item), min, max, 1, 6, 0)
-    adj.connect("value_changed", lambda adj: setter(adj, path + "/" + item))
+    adj.connect("value_changed", setter, path + "/" + item)
     hsc = gtk.HScale(adj)
     hsc.set_size_request(100, -1)
     t.attach(hsc, 1, 2, row, row+1)
 
-class PhaserWindow(gtk.Window):
-    def __init__(self, instrument, output):
+class PluginWindow(gtk.Window):
+    def __init__(self, instrument, output, plugin_name, main_window):
         gtk.Window.__init__(self, gtk.WINDOW_TOPLEVEL)
+        self.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_UTILITY)
+        self.set_transient_for(main_window)
         self.path = "/instr/%s/insert%s/engine" % (instrument, "" if output == 1 else str(output))
-        self.set_title("Phaser - %s" % instrument)
+        self.set_title("%s - %s" % (plugin_name, instrument))
+
+class PhaserWindow(PluginWindow):
+    def __init__(self, instrument, output, main_window):
+        PluginWindow.__init__(self, instrument, output, "Phaser", main_window)
         values = GetThings(self.path + "/status", ["center_freq", "mod_depth", "fb_amt", "lfo_freq", "stereo_phase", "stages", "wet_dry"], [])
         t = gtk.Table(2, 7)
         add_slider_row(t, 0, "Center", self.path, values, "center_freq", 100, 20000)
@@ -159,14 +155,12 @@ class PhaserWindow(gtk.Window):
         add_slider_row(t, 3, "LFO frequency", self.path, values, "lfo_freq", 0, 20)
         add_slider_row(t, 4, "Stereo", self.path, values, "stereo_phase", 0, 360)
         add_slider_row(t, 5, "Wet/dry", self.path, values, "wet_dry", 0, 1)
-        add_slider_row(t, 6, "Stages", self.path, values, "stages", 1, 12, setter = effect_value_changed_int)
+        add_slider_row(t, 6, "Stages", self.path, values, "stages", 1, 12, setter = adjustment_changed_int)
         self.add(t)
 
-class ChorusWindow(gtk.Window):
-    def __init__(self, instrument, output):
-        gtk.Window.__init__(self, gtk.WINDOW_TOPLEVEL)
-        self.path = "/instr/%s/insert%s/engine" % (instrument, "" if output == 1 else str(output))
-        self.set_title("Chorus - %s" % instrument)
+class ChorusWindow(PluginWindow):
+    def __init__(self, instrument, output, main_window):
+        PluginWindow.__init__(self, instrument, output, "Chorus", main_window)
         values = GetThings(self.path + "/status", ["min_delay", "mod_depth", "lfo_freq", "stereo_phase", "stages", "wet_dry"], [])
         t = gtk.Table(2, 5)
         add_slider_row(t, 0, "Min. delay", self.path, values, "min_delay", 1, 20)
@@ -176,11 +170,9 @@ class ChorusWindow(gtk.Window):
         add_slider_row(t, 4, "Wet/dry", self.path, values, "wet_dry", 0, 1)
         self.add(t)
 
-class FBRWindow(gtk.Window):
-    def __init__(self, instrument, output):
-        gtk.Window.__init__(self, gtk.WINDOW_TOPLEVEL)
-        self.path = "/instr/%s/insert%s/engine" % (instrument, "" if output == 1 else str(output))
-        self.set_title("Feedback Reducer - %s" % instrument)
+class FBRWindow(PluginWindow):
+    def __init__(self, instrument, output, main_window):
+        PluginWindow.__init__(self, instrument, output, "Feedback Reducer", main_window)
         values = GetThings(self.path + "/status", ["%active", "%center", "%q", "%gain"], [])
         t = gtk.Table(4, 17)
         cols = [
@@ -196,14 +188,14 @@ class FBRWindow(gtk.Window):
                 value = getattr(values, par[3])[j]
                 if par[4] == 'slider':
                     adj = gtk.Adjustment(value, par[1], par[2], 1, 6, 0)
-                    adj.connect("value_changed", lambda adj, param, band: effect_value_changed_array_float(adj.get_value(), self.path + "/" + param, band), par[3], j)
+                    adj.connect("value_changed", adjustment_changed_float, self.path + "/" + par[3], int(j))
                     slider = gtk.HScale(adj)
                     slider.set_size_request(120, -1)
                     t.attach(slider, i, i + 1, j + 1, j + 2, gtk.EXPAND | gtk.FILL)
                 else:
                     cb = gtk.CheckButton(par[0])
                     cb.set_active(value > 0)
-                    cb.connect("clicked", lambda cb, param, band: effect_value_changed_array_int(1 if cb.get_active() else 0, self.path + "/" + param, band), par[3], j)
+                    cb.connect("clicked", checkbox_changed_bool, self.path + "/" + par[3], int(j))
                     t.attach(cb, i, i + 1, j + 1, j + 2, gtk.EXPAND | gtk.FILL)
                 
         
@@ -224,11 +216,8 @@ class MainWindow(gtk.Window):
         self.add(self.vbox)
         self.create()
         self.refresh_id = glib.timeout_add(30, lambda: self.update())
-        
-    def create(self):
-        rt = GetThings("/rt/status", ['audio_channels'], [])
-        scene = GetThings("/scene/status", ['*layer', '*instrument', 'name', 'title'], [])
-        
+
+    def create_master(self, scene):
         self.master_info = left_label("")
         self.timesig_info = left_label("")
         
@@ -250,26 +239,33 @@ class MainWindow(gtk.Window):
         
         t.attach(bold_label("Tempo"), 0, 1, 4, 5, gtk.SHRINK | gtk.FILL)
         self.tempo_adj = gtk.Adjustment(40, 40, 300, 1, 5, 0)
-        self.tempo_adj.connect('value_changed', tempo_value_changed)
-        t.attach(gtk.HScale(self.tempo_adj), 1, 2, 4, 5)
+        self.tempo_adj.connect('value_changed', adjustment_changed_float, "/master/set_tempo")
+        t.attach(gtk.HScale(self.tempo_adj), 1, 2, 4, 5, gtk.EXPAND | gtk.FILL)
+        a = gtk.Alignment(0, 0, 1, 0)
+        a.add(t)
+        return a
+
+    def create(self):
+        rt = GetThings("/rt/status", ['audio_channels'], [])
+        scene = GetThings("/scene/status", ['*layer', '*instrument', 'name', 'title'], [])
         
-        self.vbox.add(t)
+        nb = gtk.Notebook()
+        self.vbox.add(nb)
+        nb.append_page(self.create_master(scene), gtk.Label("Master"))
         
         for i in scene.instrument:
             idata = GetThings("/instr/%s/status" % i[0], ['%gain', '%output', 'aux_offset', '%insert_engine'], [])
             #attribs = GetThings("/scene/instr_info", ['engine', 'name'], [i])
             #markup += '<b>Instrument %d:</b> engine %s, name %s\n' % (i, attribs.engine, attribs.name)
-            f = gtk.Frame(label = 'Instrument %s' % i[0])
             b = gtk.VBox(spacing = 5)
             b.set_border_width(5)
-            f.add(b)
             b.add(gtk.Label("Engine: %s" % i[1]))
             b.add(gtk.HSeparator())
             t = gtk.Table(1 + len(idata.output), 5)
-            t.attach(bold_label("Instr. output"), 0, 1, 0, 1, gtk.SHRINK)
-            t.attach(bold_label("Send to"), 1, 2, 0, 1, gtk.SHRINK)
-            t.attach(bold_label("Gain [dB]"), 2, 3, 0, 1)
-            t.attach(bold_label("Effect"), 3, 5, 0, 1)
+            t.attach(bold_label("Instr. output", 0.5), 0, 1, 0, 1, gtk.SHRINK)
+            t.attach(bold_label("Send to", 0.5), 1, 2, 0, 1, gtk.SHRINK)
+            t.attach(bold_label("Gain [dB]", 0.5), 2, 3, 0, 1)
+            t.attach(bold_label("Effect", 0.5), 3, 5, 0, 1)
             b.add(t)
             y = 1
             for o in idata.output.keys():
@@ -289,19 +285,23 @@ class MainWindow(gtk.Window):
                 cb.connect('changed', output_combo_value_changed, i[0], o)
                 t.attach(cb, 1, 2, y, y + 1, gtk.SHRINK)
                 adj = gtk.Adjustment(idata.gain[o], -96, 12, 1, 6, 0)
-                adj.connect('value_changed', gain_value_changed, i[0], o)
-                t.attach(gtk.HScale(adj), 2, 3, y, y + 1)
+                adj.connect('value_changed', adjustment_changed_float, "/instr/%s/set_gain" % i[0], int(o))
+                sc = gtk.HScale(adj)
+                sc.set_size_request(120, -1)
+                t.attach(sc, 2, 3, y, y + 1)
                 fx = gtk.Label(idata.insert_engine[o])
                 t.attach(fx, 3, 4, y, y + 1)
                 if idata.insert_engine[o] in engine_window_map:
                     fx = gtk.Button("_Edit")
                     t.attach(fx, 4, 5, y, y + 1)
-                    fx.connect("clicked", lambda button, instr, output, wclass: wclass(instr, output).show_all(), i[0], o, engine_window_map[idata.insert_engine[o]])
+                    fx.connect("clicked", lambda button, instr, output, wclass, main_window: wclass(instr, output, main_window).show_all(), i[0], o, engine_window_map[idata.insert_engine[o]], self)
                 y += 1
             if i[1] in engine_window_map:
                 b.add(gtk.HSeparator())
                 b.add(engine_window_map[i[1]](i[0], "/instr/%s/engine" % i[0]))
-            self.vbox.add(f)
+            a = gtk.Alignment(0, 0, 1, 0)
+            a.add(b)
+            nb.append_page(a, gtk.Label(i[0]))
         self.update()
         
     def update(self):
