74547ee7199f27e17aa1d2a8fe4f904528cbaa40
Krzysztof Foltman <wdev@foltman.com>
2012-09-17 22:06:59 +0100

    Make unit tests use Python unit test framework.

--- py/test.py
+++ py/test.py
@@ -1,30 +1,41 @@
 import sys
 import struct
+import unittest
 
 sys.path = [__file__[0 : __file__.rfind('/')]] + sys.path
+sys.argv = [__file__]
 
 import cbox
 
 cbox.Document.dump()
-#cbox.GetThings("/doc/uuid/blabla/bla", ['uuid'], []).uuid
-scene_uuid = cbox.GetThings("/scene/get_uuid", ['uuid'], []).uuid
-layer_uuid = cbox.GetThings("/scene/status", ['layer'], []).layer[1]
-rt_uuid = cbox.GetThings("/rt/get_uuid", ['uuid'], []).uuid
-assert cbox.GetThings(cbox.Document.uuid_cmd(scene_uuid, "/status"), ['uuid'], []).uuid == scene_uuid
-assert cbox.GetThings(cbox.Document.uuid_cmd(layer_uuid, "/status"), ['uuid'], []).uuid == layer_uuid
-assert cbox.GetThings(cbox.Document.uuid_cmd(rt_uuid, "/status"), ['uuid'], []).uuid == rt_uuid
 
-layers = cbox.GetThings("/scene/status", ['%layer'], []).layer
-assert(len(layers) == 1)
-assert(layers[1] == layer_uuid)
+class TestCbox(unittest.TestCase):
+    def test_scene(self):
+        scene_uuid = cbox.GetThings("/scene/get_uuid", ['uuid'], []).uuid
+        layer_uuid = cbox.GetThings("/scene/status", ['layer'], []).layer[1]
+        self.assertEquals(cbox.GetThings(cbox.Document.uuid_cmd(scene_uuid, "/status"), ['uuid'], []).uuid, scene_uuid)
+        self.assertEquals(cbox.GetThings(cbox.Document.uuid_cmd(layer_uuid, "/status"), ['uuid'], []).uuid, layer_uuid)
 
-instr_uuid = cbox.GetThings(cbox.Document.uuid_cmd(layer_uuid, "/status"), ['instrument_uuid'], []).instrument_uuid
-iname = cbox.GetThings(cbox.Document.uuid_cmd(layer_uuid, "/status"), ['instrument_name'], []).instrument_name
-assert cbox.GetThings("/scene/instr/%s/status" % iname, ['uuid'], []).uuid == instr_uuid
+        layers = cbox.GetThings("/scene/status", ['%layer'], []).layer
+        self.assertEquals(len(layers), 1)
+        self.assertEquals(layers[1], layer_uuid)
+        
+        instr_uuid = cbox.GetThings(cbox.Document.uuid_cmd(layer_uuid, "/status"), ['instrument_uuid'], []).instrument_uuid
+        iname = cbox.GetThings(cbox.Document.uuid_cmd(layer_uuid, "/status"), ['instrument_name'], []).instrument_name
+        self.assertEquals(cbox.GetThings("/scene/instr/%s/status" % iname, ['uuid'], []).uuid, instr_uuid)
 
-assert cbox.GetThings("/scene/instr/%s/output/1/rec_dry/status" % iname, ['*handler'], []).handler == []
-meter_uuid = cbox.GetThings("/new_meter", ['uuid'], []).uuid
-cbox.do_cmd('/scene/instr/vintage/output/1/rec_dry/attach', None, [meter_uuid])
-assert cbox.GetThings("/scene/instr/%s/output/1/rec_dry/status" % iname, ['*handler'], []).handler == [[1, meter_uuid]]
-cbox.do_cmd('/scene/instr/vintage/output/1/rec_dry/detach', None, [meter_uuid])
-assert cbox.GetThings("/scene/instr/%s/output/1/rec_dry/status" % iname, ['*handler'], []).handler == []
+    def test_rt(self):
+        rt_uuid = cbox.GetThings("/rt/get_uuid", ['uuid'], []).uuid
+        self.assertEquals(cbox.GetThings(cbox.Document.uuid_cmd(rt_uuid, "/status"), ['uuid'], []).uuid, rt_uuid)
+
+    def test_recorder_api(self):
+        layer_uuid = cbox.GetThings("/scene/status", ['layer'], []).layer[1]
+        iname = cbox.GetThings(cbox.Document.uuid_cmd(layer_uuid, "/status"), ['instrument_name'], []).instrument_name
+        self.assertEquals(cbox.GetThings("/scene/instr/%s/output/1/rec_dry/status" % iname, ['*handler'], []).handler, [])
+        meter_uuid = cbox.GetThings("/new_meter", ['uuid'], []).uuid
+        cbox.do_cmd('/scene/instr/vintage/output/1/rec_dry/attach', None, [meter_uuid])
+        self.assertEquals(cbox.GetThings("/scene/instr/%s/output/1/rec_dry/status" % iname, ['*handler'], []).handler, [[1, meter_uuid]])
+        cbox.do_cmd('/scene/instr/vintage/output/1/rec_dry/detach', None, [meter_uuid])
+        self.assertEquals(cbox.GetThings("/scene/instr/%s/output/1/rec_dry/status" % iname, ['*handler'], []).handler, [])
+        
+unittest.main()
