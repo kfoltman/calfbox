9984234ca2f0ebb202c6c1d2b7d42e0d43a1eead
Krzysztof Foltman <wdev@foltman.com>
2012-10-21 10:08:47 +0100

    More Nocturn work.

---
+++ novabox.py
@@ -0,0 +1,111 @@
+# A primitive drum machine interface for Novation Nocturn.
+#
+# Usage:
+#   - make sure that Novation Nocturn is connected *and* that the USB device
+#     that corresponds to it can be opened by the current user
+#   - create an ini file containing a scene with a single instrument using
+#     a sampler or fluidsynth engine + some drum mappings in SFZ or SF2
+#   - ensure that the ini file contains 7 drum patterns, pat1..pat7, these
+#     can be copied from cboxrc-example
+#   - user buttons 1..7 start drum patterns
+#   - user button 8 stops the playback
+#   - encoder knob 1 adjusts the volume
+#   - mixer button exits the application
+
+import pygtk
+import gtk
+import glib
+import gobject
+import math
+import sys
+
+sys.path = ["./py"] + sys.path
+
+import nocturn
+import cbox
+
+quit = False
+
+instr_name = cbox.Document.get_scene().status().instruments.keys()[0]
+
+def clamp(val, min, max):
+    if val < min:
+        val = min
+    elif val > max:
+        val = max
+    return val
+
+class NovaBox:
+    def __init__(self):
+        self.nocturn = nocturn.Nocturn()
+        self.cur_pattern = None
+        self.handlers = {}
+        self.handlers[83] = self.on_xfade_touch
+        for i in range(7):
+            self.handlers[112 + i] = lambda cmd, val: self.on_buttonN_press(cmd - 112) if val > 0 else None
+        self.handlers[112 + 7] = lambda cmd, val: self.on_button8_press() if val > 0 else None
+        self.handlers[64] = self.on_knob1_change
+        self.handlers[65] = self.on_knob2_change
+        self.handlers[127] = lambda cmd, val: self.on_mixer_press() if val > 0 else None
+
+    def on_knob1_change(self, cmd, val):
+        scene = cbox.Document.get_scene()
+        instr = scene.status().instruments[instr_name][1]
+        gain = instr.get_things('/output/1/status', ['gain']).gain
+        if val > 63:
+            val = -128 + val
+        instr.cmd('/output/1/gain', None, gain + val * 0.5)
+
+    def on_knob2_change(self, cmd, val):
+        tempo = cbox.GetThings("/master/status", ['tempo'], []).tempo
+        if val > 63:
+            val = -128 + val
+        tempo = clamp(tempo + val * 0.5, 30, 300)
+        cbox.do_cmd('/master/set_tempo', None, [tempo])
+
+    def on_buttonN_press(self, button):
+        cbox.do_cmd("/master/stop", None, [])
+        song = cbox.Document.get_song()
+        song.loop_single_pattern(lambda: song.load_drum_pattern('pat%d' % (button + 1)))
+        cbox.do_cmd("/master/seek_ppqn", None, [0])
+        cbox.do_cmd("/master/play", None, [])
+        self.cur_pattern = button
+
+    def on_button8_press(self):
+        self.cur_pattern = None
+        cbox.do_cmd("/master/stop", None, [])
+        
+    def on_mixer_press(self):
+        global quit
+        quit = True
+
+    def on_xfade_touch(self, cmd, val):
+        if val > 0:
+            print "Do not touch"
+
+    def handler(self, cmd, val):
+        if cmd in self.handlers:
+            self.handlers[cmd](cmd, val)
+            return
+
+    def poll(self):
+        self.nocturn.poll(self.handler)
+        
+    def update(self):
+        scene = cbox.Document.get_scene()
+        cmds = nocturn.NocturnCommands()
+        master = cbox.GetThings("/master/status", ['playing'], [])
+        for i in range(7):
+            cmds.setModeButtonLight(i, self.cur_pattern == i)
+        gain = scene.status().instruments[instr_name][1].get_things('/output/1/status', ['gain']).gain
+        cmds.setEncoderMode(0, 0)
+        cmds.setEncoderValue(0, clamp(int(gain * 2 + 64), 0, 127))
+        
+        cmds.setModeButtonLight(7, self.cur_pattern is None)
+        self.nocturn.execute(cmds)
+
+nb = NovaBox()
+while not quit:
+    nb.poll()
+    nb.update()
+nb.nocturn.reset()
---
+++ py/nocturn.py
@@ -0,0 +1,122 @@
+# Novation Nocturn driver
+# Based on DWTFYW code by De Wet van Niekert (dewert). However, I had to
+# put reading of the input endpoint in a separate thread because the only
+# reliable way to read it is by using large timeouts (1s or so). With shorter
+# timeouts, some events are lost/replaced by crossfader value.
+
+import array
+import binascii
+import fcntl
+import os
+import usb.core
+import usb.util
+import sys
+import time
+import threading
+
+class NocturnCommands:
+    def __init__(self):
+        self.pkt = ""
+    def setModeButtonLight(self, button, state):
+        self.pkt += chr(0x70 + button) + ('\x01' if state else '\x00')
+    def setUserButtonLight(self, button, state):
+        self.pkt += chr(0x78 + button) + ('\x01' if state else '\x00')
+    def setEncoderMode(self, encoder, mode):
+        self.pkt += chr(0x48 + encoder) + chr(mode << 4)
+    def setEncoderValue(self, encoder, value):
+        self.pkt += chr(0x40 + encoder) + chr(value)
+    def setSpeedDialMode(self, mode):
+        self.pkt += chr(0x51) + chr(mode << 4)
+    def setSpeedDialValue(self, value):
+        self.pkt += chr(0x50) + chr(value)
+    def clear(self):
+        for i in range(8):
+            self.setModeButtonLight(i, False)
+            self.setUserButtonLight(i, False)
+            if i & 1:
+                self.setEncoderMode(i, 3)
+            else:
+                self.setEncoderMode(i, 4)
+            self.setEncoderValue(i, 64)
+        self.setSpeedDialMode(5)
+        self.setSpeedDialValue(64)
+
+class NocturnHandler(threading.Thread):
+    def __init__(self, n):
+        threading.Thread.__init__(self)
+        self.nocturn = n
+        self.rpipefd, self.wpipefd = os.pipe()
+        self.rpipe = os.fdopen(self.rpipefd, "rb")
+        self.wpipe = os.fdopen(self.wpipefd, "wb")
+        flags = fcntl.fcntl(self.rpipe, fcntl.F_GETFL)
+        fcntl.fcntl(self.rpipe, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+        self.setDaemon(True)
+    def run(self):
+        while True:
+            pkt = self.nocturn.read()
+            if pkt is not None:
+                self.wpipe.write(pkt)
+                self.wpipe.flush()
+    def poll(self, handler):
+        try:
+            data = array.array('B', self.rpipe.read())
+            i = 0
+            # For longer sequencer, Nocturn skips the control change message
+            while i < len(data):
+                if data[i] == 176:
+                    i += 1
+                    continue
+                handler(data[i], data[i + 1])
+                i += 2
+        except IOError, e:
+            pass
+    def get_poll_fd(self):
+        return self.rpipefd
+
+class Nocturn:
+    vendorID = 0x1235
+    productID = 0x000a
+    def __init__(self):
+        dev = usb.core.find(idVendor=self.vendorID, idProduct=self.productID)
+        # The values in here don't seem to matter THAT much
+        initPackets=["b00000","28002b4a2c002e35","2a022c722e30"]
+        #This is a minimum set that enables the device, but then it doesn't
+        #really work reliably, at least the touch sensing
+        #initPackets=["b00000", "2800"]
+
+        if dev is None:
+            raise ValueError('Device not found')
+            sys.exit()
+
+        self.dev = dev
+        cfg = dev[1]
+        intf = cfg[(0,0)]
+
+        self.ep = intf[1]
+        self.ep2 = intf[0]
+        dev.set_configuration(2)
+        for packet in initPackets:
+            self.ep.write(binascii.unhexlify(packet))
+        
+        self.reset()
+        self.reader = NocturnHandler(self)
+        self.reader.start()
+
+    def reset(self):
+        cmd = NocturnCommands()
+        cmd.clear()
+        self.execute(cmd)
+
+    def execute(self, cmd):
+        self.ep.write(cmd.pkt)
+    def read(self):
+        try:
+            data = self.ep2.read(8, None)
+            return buffer(data)
+        except usb.core.USBError, e:
+            return None
+    def poll(self, handler):
+        return self.reader.poll(handler)
+    def get_poll_fd(self):
+        return self.reader.get_poll_fd()
+
