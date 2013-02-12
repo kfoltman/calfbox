8cc7db8603ea418a874482d3a6871ba4068dfa23
Krzysztof Foltman <wdev@foltman.com>
2013-02-12 20:25:20 +0000

    Add an example of the new modulations.

--- sampler_api_example.py
+++ sampler_api_example.py
@@ -29,15 +29,18 @@ g1.set_param("fileg_release", "10")
 g1.set_param("ampeg_release", "0.1")
 g1.set_param("amp_veltrack", "0")
 g1.set_param("volume", "-12")
+g1.set_param("fileg_depthcc14", "-5400")
 
 r1 = g1.new_region()
 r1.set_param("sample", "*saw")
 r1.set_param("transpose", "0")
 r1.set_param("tune", "5")
+r1.set_param("gain_cc17", "12")
 
 r2 = g1.new_region()
 r2.set_param("sample", "*sqr")
 r2.set_param("transpose", "12")
+r2.set_param("gain_cc17", "-12")
 
 print "Ready!"
 
