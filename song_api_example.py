7bd7dd4e6c06d88fbdf4356bf1ee437caff56470
Krzysztof Foltman <wdev@foltman.com>
2013-02-25 21:56:35 +0000

    Add an example demonstrating use of sequencer API.

---
+++ song_api_example.py
@@ -0,0 +1,63 @@
+import os
+import sys
+import struct
+import time
+import unittest
+
+sys.path = ["./py"] + sys.path
+
+import cbox
+
+global Document
+Document = cbox.Document
+
+song = Document.get_song()
+
+# Delete all the tracks and patterns
+song.clear()
+
+# Add the first track
+trk = song.add_track()
+
+# Create a binary blob that contains the MIDI events
+pblob = bytes()
+for noteindex in range(10):
+    # note on
+    pblob += cbox.Pattern.serialize_event(noteindex * 24, 0x90, 36, 127)
+    # note off
+    pblob += cbox.Pattern.serialize_event(noteindex * 24 + 23, 0x90, 36, 0)
+
+# This will be the length of the pattern (in pulses). It should be large enough
+# to fit all the events
+pattern_len = 10 * 24
+
+# Create a new pattern object using events from the blob
+pattern = song.pattern_from_blob(pblob, pattern_len)
+
+# Add an instance (clip) of the pattern to the track at position 0
+# The clip will contain the whole pattern (it is also possible to insert
+# a single slice of the pattern)
+clip = trk.add_clip(0, 0, pattern_len, pattern)
+
+# Stop the song at the end
+song.set_loop(pattern_len, pattern_len)
+
+# Send the updated song data to the realtime thread
+song.update_playback()
+
+# The /master object API doesn't have any nice Python wrapper yet, so accessing
+# it is a bit ugly, still - it works
+
+# Set tempo - the argument must be a float
+cbox.do_cmd("/master/set_tempo", None, [120.0])
+# Start playback
+cbox.do_cmd("/master/play", None, [])
+print ("Playing")
+
+while True:
+    # Get transport information - current position (samples and pulses), current tempo etc.
+    master = cbox.GetThings("/master/status", ['pos', 'pos_ppqn', 'tempo', 'timesig', 'sample_rate'], [])
+    print (master.pos_ppqn)
+    # Query JACK ports, new USB devices etc.
+    cbox.do_cmd("/on_idle", None, [])
+    time.sleep(0.1)
