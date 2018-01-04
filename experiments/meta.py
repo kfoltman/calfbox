239ce8120e00322a84154801470f6bbff1fbc65e
Nils <nils@hilbricht.net>
2018-01-04 21:08:59 +0100

    Add GPL header to the experiments

--- experiments/meta.py
+++ experiments/meta.py
@@ -1,5 +1,23 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Copyright 2017, Nils Hilbricht, Germany ( https://www.hilbricht.net )
+
+This code is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
 import re
 from calfbox import cbox  #use the globally installed calfbox
 from asyncio import get_event_loop
