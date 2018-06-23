ef8ea8d1570a8c4f4d2c217419ea4501abd7b6dc
Nils <>
2018-06-23 16:36:01 +0200

    Nullbox: Handle importing cbox or calfbox in subsequent files. Now it is enough to simply call 'import calfbox.nullbox' before everything else

--- py/nullbox.py
+++ py/nullbox.py
@@ -1,9 +1,18 @@
 class NullCalfbox(str): #iterable
     """A drop-in replacement for calfboxs python module.
-    Replace your
+    Use this for testing and development.
+
+    At the start of your program, first file, insert:
+        import calfbox.nullbox
+
+    All further
         from calfbox import cbox
-    with
-        from calfbox.nullbox import cbox"""
+    will use the null module.
+
+    Even additional
+        import calfbox
+    will use the nullbox module.
+    """
 
     def __init__(self, *args):
         self.client_name = ""
@@ -17,5 +26,10 @@ class NullCalfbox(str): #iterable
 
     def serialize_event(self, *args):
         return b''
+
+import sys
+import calfbox.nullbox
+sys.modules["calfbox"] = sys.modules["calfbox.nullbox"]
+import calfbox
 
 cbox = NullCalfbox("fake cbox null client")
