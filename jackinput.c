4b13e7a7e68c41184416b1af6410be941bb4b9b0
Krzysztof Foltman <wdev@foltman.com>
2019-10-24 20:39:27 +0100

    Add support for unconnected input and Python scripting to the JACK input "instrument".

--- jackinput.c
+++ jackinput.c
@@ -50,20 +50,67 @@ void jack_input_process_block(struct cbox_module *module, cbox_sample_t **inputs
     
     for (int i = 0; i < 2; i++)
     {
-        float *src = module->rt->io->input_buffers[m->inputs[i]] + m->offset;
-        for (int j = 0; j < CBOX_BLOCK_SIZE; j++)
-            outputs[i][j] = src[j];
+        if (m->inputs[i] < 0)
+        {
+            for (int j = 0; j < CBOX_BLOCK_SIZE; j++)
+                outputs[i][j] = 0;
+        }
+        else
+        {
+            float *src = module->rt->io->input_buffers[m->inputs[i]] + m->offset;
+            for (int j = 0; j < CBOX_BLOCK_SIZE; j++)
+                outputs[i][j] = src[j];
+        }
     }
     m->offset = (m->offset + CBOX_BLOCK_SIZE) % app.io.io_env.buffer_size;
 }
 
 static gboolean validate_input_index(int input, const char *cfg_section, const char *type, GError **error)
 {
-    if (input < 1 || input > (int)app.io.io_env.input_count)
+    if ((input < 1 || input > (int)app.io.io_env.input_count) && input != -1)
     {
-        g_set_error(error, CBOX_MODULE_ERROR, CBOX_MODULE_ERROR_OUT_OF_RANGE, "%s: invalid value for %s (%d), allowed values are 1..%d", cfg_section, type, input, app.io.io_env.input_count);
+        g_set_error(error, CBOX_MODULE_ERROR, CBOX_MODULE_ERROR_OUT_OF_RANGE, "%s: invalid value for %s (%d), allowed values are 1..%d or -1 for unconnected", cfg_section, type, input, app.io.io_env.input_count);
         return FALSE;
     }
+    return TRUE;
+}
+
+static void jack_input_destroyfunc(struct cbox_module *module)
+{
+}
+
+static int to_base1(int val)
+{
+    if (val < 0)
+        return val;
+    return 1 + val;
+}
+
+gboolean jack_input_process_cmd(struct cbox_command_target *ct, struct cbox_command_target *fb, struct cbox_osc_command *cmd, GError **error)
+{
+    struct jack_input_module *m = ct->user_data;
+    if (!strcmp(cmd->command, "/status") && !strcmp(cmd->arg_types, ""))
+    {
+        if (!cbox_check_fb_channel(fb, cmd->command, error))
+            return FALSE;
+        if (!cbox_execute_on(fb, NULL, "/inputs", "ii", error, to_base1(m->inputs[0]), to_base1(m->inputs[1])))
+            return FALSE;
+        return CBOX_OBJECT_DEFAULT_STATUS(&m->module, fb, error);
+    }
+    else if (!strcmp(cmd->command, "/inputs") && !strcmp(cmd->arg_types, "ii"))
+    {
+        int left_input = CBOX_ARG_I(cmd, 0);
+        int right_input = CBOX_ARG_I(cmd, 1);
+        if (!validate_input_index(left_input, "script", "left input", error))
+            return FALSE;
+        if (!validate_input_index(right_input, "script", "right input", error))
+            return FALSE;
+        m->inputs[0] = left_input < 0 ? -1 : left_input - 1;
+        m->inputs[1] = right_input < 0 ? -1 : right_input - 1;
+        return TRUE;
+    }
+    else
+        return cbox_object_default_process_cmd(ct, fb, cmd, error);
     return TRUE;
 }
 
@@ -83,7 +130,7 @@ MODULE_CREATE_FUNCTION(jack_input)
         return NULL;
     
     struct jack_input_module *m = malloc(sizeof(struct jack_input_module));
-    CALL_MODULE_INIT_SIMPLE(m, 0, 2);
+    CALL_MODULE_INIT(m, 0, 2, jack_input);
     m->module.process_event = jack_input_process_event;
     m->module.process_block = jack_input_process_block;
     
